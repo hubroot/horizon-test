45926ebb7bc0f7281cdd49568ba0a5bab812c9da
l <viroxic@gmail.com>
2019-10-19 07:40:12 +0100

    added initial app routes

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,10 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+
 import Home from '../views/Home.vue'
+import Map from '../views/Map.vue'
+import Form from '../views/Form.vue'
+import Editor from '../views/Editor.vue'
 
 Vue.use(VueRouter)
 
@@ -11,12 +15,19 @@ const routes = [
     component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    path: '/map',
+    name: 'map',
+    component: Map
+  },
+  {
+    path: '/form',
+    name: 'form',
+    component: Form
+  },
+  {
+    path: '/editor',
+    name: 'editor',
+    component: Editor
   }
 ]
 
