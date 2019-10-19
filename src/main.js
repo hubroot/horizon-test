35a01aa9b4d6986717954fa937ffda0dcf1346e8
l <viroxic@gmail.com>
2019-10-20 00:48:41 +0100

    jquery not needeed there

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
-import 'jquery'
 import 'popper.js'
 import 'bootstrap'
 import 'bootstrap/dist/css/bootstrap.min.css'
